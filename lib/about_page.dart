0cb590d537c2bedbb331e2d64771c9405e74f85b
Raymond_Law2 <moonshadow278@gmail.com>
2023-09-06 06:38:21 +0000

    Local images can be displayed

--- lib/about_page.dart
+++ lib/about_page.dart
@@ -4,20 +4,25 @@ class AboutPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text("About")),
-        body: Center(
-            child:
-                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-          // Text("App Name"),
-          Image.asset('route_demo.jpg'),
-          SizedBox(height: 16),
-          Text("Version 1.0.0"),
-          SizedBox(height: 16),
-          ElevatedButton(
-              child: Text("Close"),
-              onPressed: () {
-                Navigator.pop(context);
-              })
-        ])));
+      appBar: AppBar(title: Text("About")),
+      // body: Center(
+      //     child:
+      //         Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+      //   // Text("App Name"),
+      //   Image.asset('images/route_demo.jpg'),
+      //   SizedBox(height: 16),
+      //   Text("Version 1.0.0"),
+      //   SizedBox(height: 16),
+      //   ElevatedButton(
+      //       child: Text("Close"),
+      //       onPressed: () {
+      //         Navigator.pop(context);
+      //       })
+      // ]))
+
+      body: Center(
+        child: Image(image: AssetImage('images/route_demo.jpg')),
+      ),
+    );
   }
 }
