1845edc5870f8b2c4622f93bc60400fb3b296c2e
Raymond_Law2 <moonshadow278@gmail.com>
2023-09-06 21:38:31 +0000

    tru end of day4

--- lib/about_page.dart
+++ lib/about_page.dart
@@ -1,31 +1,55 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class AboutPage extends StatelessWidget {
+class AboutPage extends StatefulWidget {
+  @override
+  _AboutPageState createState() => _AboutPageState();
+}
+
+class _AboutPageState extends State<AboutPage> {
+  final Completer<GoogleMapController> _controller = Completer();
+  static const CameraPosition _london = CameraPosition(
+    target: LatLng(51.5071943, -0.102977),
+    zoom: 15,
+  );
+  MapType _currentMapType = MapType.normal;
+  void _onMapType() {
+    setState(() {
+      _currentMapType = _currentMapType == MapType.normal
+          ? MapType.satellite
+          : MapType.normal;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    // return const Center(
-    //   child: CircularProgressIndicator(),
-    // );
-
     return Scaffold(
-        appBar: AppBar(title: Text("About")),
-        // body: Center(
-        //     child:
-        //         Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-        //   // Text("App Name"),
-        //   Image.asset('images/route_demo.jpg'),
-        //   SizedBox(height: 16),
-        //   Text("Version 1.0.0"),
-        //   SizedBox(height: 16),
-        //   ElevatedButton(
-        //       child: Text("Close"),
-        //       onPressed: () {
-        //         Navigator.pop(context);
-        //       })
-        // ]))
-
-        body: Center(
-          child: Image(image: AssetImage('images/route_demo.jpg')),
-        ));
+      appBar: AppBar(
+        title: const Text('Google Map Sample'),
+        backgroundColor: Colors.blue,
+      ),
+      body: Stack(
+        children: [
+          GoogleMap(
+            mapType: _currentMapType,
+            initialCameraPosition: _london,
+            onMapCreated: (GoogleMapController controller) {
+              _controller.complete(controller);
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.all(18),
+            child: Align(
+              alignment: Alignment.topRight,
+              child: FloatingActionButton(
+                onPressed: _onMapType,
+                child: const Icon(Icons.map, size: 36),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
